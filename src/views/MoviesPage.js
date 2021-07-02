b214e83dfaf600000ca7f85dbf628c02e957e6d2
AntonLovkin <anton28111987@gmail.com>
2021-07-02 16:26:00 +0300

    search working

--- src/views/MoviesPage.js
+++ src/views/MoviesPage.js
@@ -1,41 +1,29 @@
-import React from 'react';
-import { Component } from "react";
+import React, { Component } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
-
-const API_KEY = "3648f672b1c21855bded6f7b57b6e29a"
+import API from '../API';
+import RenderMovies from './RenderMovies';
 
 class MoviesPage extends Component {
     state = {
-        movies: [],
+        movies: JSON.parse(localStorage.getItem('movies')),
         value: '',
     }
 
-    async componentDidMount() {
-        
-        const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
-            // .then(response => this.setState({ movies: response.data.results }))
-        // console.log(response.data.results);
-
-        // console.log(this.state.movies);
-
-        this.setState({movies: response.data.results})
-    }
+     renderMovies = (e) => {
+        e.preventDefault();
+        API.fetchMoviesWithSearchQuery(this.state.value).then(movies => {
+            this.setState({ movies: movies });
+            localStorage.setItem('movies', JSON.stringify(movies));
+            console.log(this.state.movies);
+            if (this.state.movies.length === 0) {
+            alert('We could not find movies with this query')
+            };
+        });
+    };
 
     setValue = (e) => {
         this.setState({ value: e.target.value });
     };
-    
-    // renderMovies = (e) => {
-    //     e.preventDefault();
-    //     API.fetchMoviesWithSearchQuery(this.state.value).then(movies => {
-    //         this.setState({ movies: movies });
-    //         localStorage.setItem('movies', JSON.stringify(movies));
-    //         if (this.state.movies.length === 0) {
-    //         alert('We could not find movies with this query')
-    //         };
-    //     });
-    // };
 
     render() {
         const { value, movies } = this.state;
@@ -46,11 +34,49 @@ class MoviesPage extends Component {
                     <input type="text" value={value} onChange={this.setValue} className='input'/>
                     <button type='submit'>Search</button>
                 </form>
-                {/* {movies.length > 0 && <RenderMovies movies={movies} />} */}
+                {movies.length > 0 && <RenderMovies movies={movies} />}
             </>
         );
     };
 }
 
 export default MoviesPage;
+
+// const API_KEY = "3648f672b1c21855bded6f7b57b6e29a"
+
+// class MoviesPage extends Component {
+//     state = {
+//         movies: [],
+//         value: '',
+//     }
+
+//     async componentDidMount() {
+        
+//         const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
+//             // .then(response => this.setState({ movies: response.data.results }))
+//         // console.log(response.data.results);
+
+//         // console.log(this.state.movies);
+
+//         this.setState({movies: response.data.results})
+//     }
+
+//     setValue = (e) => {
+//         this.setState({ value: e.target.value });
+//     };
+    
+//     render() {
+//         const { value, movies } = this.state;
+//         console.log(movies);
+//         return (
+//             <>
+//                 <form onSubmit={this.renderMovies}>
+//                     <input type="text" value={value} onChange={this.setValue} className='input'/>
+//                     <button type='submit'>Search</button>
+//                 </form>
+//                 {/* {movies.length > 0 && <RenderMovies movies={movies} />} */}
+//             </>
+//         );
+//     };
+// }
 
---
+++ src/views/RenderMovies.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+// import './RenderMovies.scss'
+
+function RenderMovies({ movies, location }) {
+    return (
+        <ul>{movies.map(({id, title, name}) =>
+        (<li key={id} >
+            <Link to={{
+                pathname: `movies/${id}`,
+                state: {from: location}
+            }} className='movie-link'> {title ? title : name}
+             </Link>
+        </li>))
+        }
+        </ul >
+    );
+};
+
+export default withRouter(RenderMovies);
