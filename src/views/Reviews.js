c7f7a299fe15bfa2de464a5dd0282de11dc40c25
AntonLovkin <anton28111987@gmail.com>
2021-07-02 16:08:22 +0300

    render Rewiews

--- src/views/Reviews.js
+++ src/views/Reviews.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { Component } from "react";
-// import axios from 'axios';
-// import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import API from '../API';
 
 class Reviews extends Component {
-    state = {}
+    state = {
+        reviews: []
+    }
 
-    render() {
-               
+    componentDidMount() {
+        API.fetchMovieReviews(this.props.match.params.movieId).then(reviews => this.setState({ reviews: reviews }))
+        console.log(this.state.reviews);
+        console.log(this.props.match.params.movieId);
+    }
+
+    render() {       
         return (
             <>
                 <h1>Reviews List</h1>
+                {this.state.reviews.length > 0
+                ? <ul>
+                    {this.state.reviews.map(({id, author, content}) => (
+                        <li key={id}>
+                            <h3>{author}</h3>
+                            <p>{content}</p>
+                        </li>
+                    ))}
+                </ul>
+                : <h1>Hello!! There is no reviews yet</h1>}
             </>
         )
     }
