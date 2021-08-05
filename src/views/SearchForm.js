aa00d645343b168cf1003c673b3d73c51e8f1ee4
AntonLovkin <anton28111987@gmail.com>
2021-08-06 00:44:03 +0300

    Create SearchForm.js

---
+++ src/views/SearchForm.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = e => {
+    this.setState({ query: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+    // this.reset();
+  };
+
+  render() {
+    const { query } = this.state;
+    return (
+      <header className="Searchbar">
+        <form onSubmit={this.handleSubmit} className="SearchForm">
+          <input
+            className="SearchForm-input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            // placeholder="Search movie"
+            onChange={this.handleChange}
+            value={query}
+          />
+          <button type="submit" className="SearchForm-button">
+            Search
+          </button>
+        </form>
+      </header>
+    );
+  }
+}
+
+export default SearchForm;
