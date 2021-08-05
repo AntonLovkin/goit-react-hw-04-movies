4901eaebcd5bd8fa9279d09825ce6df1a6185f10
AntonLovkin <anton28111987@gmail.com>
2021-08-06 00:44:01 +0300

    Update RenderMovies.js

--- src/views/RenderMovies.js
+++ src/views/RenderMovies.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
-// import './RenderMovies.scss'
 
 function RenderMovies({ movies, location }) {
     return (
-        <ul>{movies.map(({id, title, name}) =>
-        (<li key={id} >
-            <Link onClick={this.onCategoryChange}
+        <ul>
+            {movies.map(({ id, title, name }) => (
+                <li key={id} >
+            <Link
                 to={{
                 pathname: `movies/${id}`,
                 state: {from: location}
                 }}
                 className='movie-link'> {title ? title : name}
-             </Link>
-        </li>))
-        }
+            </Link>
+                </li>
+            ))
+           }
         </ul >
     );
 };
